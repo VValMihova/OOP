d739d93f59ce6db327d8079b1c993a4f153cef40
VValMihova <mishomishkatamishunchev666@gmail.com>
2023-07-01 19:55:36 +0300

    02. Hierarchical Inheritance

---
+++ Inheritance_03/Lab/p02HierarchicalInheritance/Animal.java
@@ -0,0 +1,7 @@
+package p02HierarchicalInheritance;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ Inheritance_03/Lab/p02HierarchicalInheritance/Cat.java
@@ -0,0 +1,7 @@
+package p02HierarchicalInheritance;
+
+public class Cat extends Animal {
+    public void meow(){
+        System.out.println("meowing...");
+    }
+}
