4e5b093364b2e13fea48a68fa0f402fb992edc97
VValMihova <mishomishkatamishunchev666@gmail.com>
2023-07-04 21:40:24 +0300

    01. Car Shop

---
+++ InterfacesAndAbstraction_04/Lab/car/Car.java
@@ -0,0 +1,11 @@
+package InterfacesAndAbstraction_04.Lab.car;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    Integer TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+}
