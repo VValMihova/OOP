7511951d3c3db98cd6e7ce0016e0f6d675b84d2c
VValMihova <mishomishkatamishunchev666@gmail.com>
2023-07-20 12:25:45 +0300

    01. Reflection

---
+++ ReflectionAndAnnotation_07/Lab/p01Reflection/Main.java
@@ -0,0 +1,21 @@
+package ReflectionAndAnnotation_07.Lab.p01Reflection;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class reflection = Reflection.class;
+        System.out.println(reflection);
+
+        Class superClass = reflection.getSuperclass();
+        System.out.println(superClass);
+
+        Class[] interfaces = reflection.getInterfaces();
+        for (Class anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+
+        Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
